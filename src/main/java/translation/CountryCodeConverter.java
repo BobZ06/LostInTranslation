643c9338b95e5979b391d4155c5549340aab59da
Mertali Tercan <mertali.tercan@gmail.com>
2025-09-22 18:05:08 -0400

    Task and fix also added .idea to gitignore

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,6 +41,7 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
+                String country = parts[0];
                 // TODO Task B: use parts to populate the instance variables
             }
         }
@@ -56,7 +57,6 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
         return code;
     }
 
@@ -66,8 +66,7 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return country;
+        return countryToCountryCode.getOrDefault(country, "")
     }
 
     /**
@@ -75,7 +74,6 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
-        return 0;
+        return countryCodeToCountry.size();
     }
 }
